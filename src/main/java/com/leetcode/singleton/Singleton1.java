26deb3bc415d70ce5702549a7e3efe54471658e2
huangshengbo <huangshengbo@mozat.com>
2018-10-20 01:42:08 +0800

    单例模式总结

---
+++ src/main/java/com/leetcode/singleton/Singleton1.java
@@ -0,0 +1,18 @@
+package com.leetcode.singleton;
+
+/**
+ * 只适用于单线程环境
+ */
+public class Singleton1 {
+    private Singleton1() {
+    }
+
+    private static Singleton1 instance = null;
+
+    public static Singleton1 getInstance() {
+        if (instance == null) {
+            instance = new Singleton1();
+        }
+        return instance;
+    }
+}
---
+++ src/main/java/com/leetcode/singleton/Singleton4.java
@@ -0,0 +1,20 @@
+package com.leetcode.singleton;
+
+/**
+ * 只有真正需要的时候才创建对象,利用类内部类和静态变量的特性.
+ */
+public class Singleton4 {
+    private Singleton4() {
+    }
+
+    public static Singleton4 getInstance() {
+        return InnerSingleton.singleton;
+    }
+
+    static class InnerSingleton {
+        private InnerSingleton() {
+        }
+
+        static Singleton4 singleton = new Singleton4();
+    }
+}
